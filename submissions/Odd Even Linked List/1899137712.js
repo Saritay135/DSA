8bcc14ffe34a7981f463066c66a19a4aea12d701
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-01-28 00:48:32 -0600

    Add submission 1899137712

---
+++ submissions/Odd Even Linked List/1899137712.js
@@ -0,0 +1,29 @@
+# Title: Odd Even Linked List
+# Submission ID: 1899137712
+# Status: Runtime Error
+# Date: January 27, 2026 at 03:37:12 PM CST
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    let odd = head;
+    let even = head.next;
+    let evenStart = even;
+    while(odd.next && even.next){
+        odd.next = odd.next.next;
+        odd = odd.next;
+        even.next = even.next.next;
+        even = even.next;
+    }
+     odd.next = evenStart;
+     return head;
+};
