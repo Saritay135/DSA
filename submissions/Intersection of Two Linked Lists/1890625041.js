b99e4b5c380a42f4d63fa69c5c8b468034cd8550
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-01-28 00:48:38 -0600

    Add submission 1890625041

---
+++ submissions/Intersection of Two Linked Lists/1890625041.js
@@ -0,0 +1,34 @@
+# Title: Intersection of Two Linked Lists
+# Submission ID: 1890625041
+# Status: Accepted
+# Date: January 19, 2026 at 11:15:35 PM CST
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let seenNode = new Set();
+    let curr = headB;
+    while(curr){
+        seenNode.add(curr);
+        curr = curr.next
+    }
+    let curr2 = headA;
+    while(curr2){
+        if(seenNode.has(curr2)){
+            return curr2;
+        }
+        curr2 = curr2.next;
+    }
+    return null;
+};
