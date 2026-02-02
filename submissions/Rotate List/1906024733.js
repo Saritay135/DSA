a66128511e11b9002ac2add40960202a045797eb
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-02-02 16:52:54 -0600

    Add submission 1906024733

---
+++ submissions/Rotate List/1906024733.js
@@ -0,0 +1,43 @@
+# Title: Rotate List
+# Submission ID: 1906024733
+# Status: Accepted
+# Date: February 2, 2026 at 04:52:47 PM CST
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    if(!head || !head.next) return head;
+    let length = 0;
+    let sentinel = new ListNode();
+    sentinel.next = head;
+    let slow = head;
+    let fast = head;
+    let curr = head;
+    while(curr){
+        curr = curr.next;
+        length ++;
+    }
+    let remainder = k%length;
+    for(let i=0; i<remainder; i++){
+        fast= fast.next;
+    }
+    while(fast.next){
+        slow = slow.next;
+        fast = fast.next;
+    }
+    fast.next = head;
+    let newhead = slow.next;
+     slow.next = null;
+
+    return newhead;;
+};
