60b5eaf142b60dd75f796807a19cd51570347eab
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-02-03 23:39:07 -0600

    Add submission 1907541087

---
+++ submissions/Swap Nodes in Pairs/1907541087.js
@@ -0,0 +1,35 @@
+# Title: Swap Nodes in Pairs
+# Submission ID: 1907541087
+# Status: Accepted
+# Date: February 3, 2026 at 11:39:00 PM CST
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    let dummy = new ListNode();
+    dummy.next = head;
+    if(!head || !head.next) return head;
+    let curr = head;
+    let prev = dummy;
+    let n = head.next;
+  
+    while(curr && curr.next){
+       let  n = curr.next; 
+        prev.next = n;
+        curr.next = n.next;
+        n.next = curr;
+        prev = prev.next.next;
+        curr = prev.next;
+        
+    }
+    return dummy.next;
+};
