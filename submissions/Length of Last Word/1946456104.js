9e3975e1162d45f28cb32e7b9d99cd3d7f076fb8
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-03-12 16:05:27 -0500

    Add submission 1946456104

---
+++ submissions/Length of Last Word/1946456104.js
@@ -0,0 +1,24 @@
+# Title: Length of Last Word
+# Submission ID: 1946456104
+# Status: Accepted
+# Date: March 12, 2026 at 04:05:21 PM CDT
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let n = s.length -1;
+    let count = 0;
+    while(n>=0){
+
+      if(s[n]!=" "){
+        count++
+      }
+        else if(count>0){
+            break;
+        }
+        --n;
+    }
+    return count;
+};
