31f773929bc26f8b0c45d6a50b1f51ff6ee929c4
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-03-17 17:32:28 -0500

    Add submission 1951569894

---
+++ submissions/Reverse String II/1951569894.js
@@ -0,0 +1,24 @@
+# Title: Reverse String II
+# Submission ID: 1951569894
+# Status: Accepted
+# Date: March 17, 2026 at 05:32:21 PM CDT
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+     s = s.split("");
+    for(let i=0; i<s.length; i=(i+(2*k))){
+        
+        let mid = Math.floor((k)/2);
+        for(let j=0; j < mid;  j++){
+           let temp = s[j+i];
+           s[i+j] = s[i+k-1-j];
+           s[i+k-j-1] = temp;
+           
+        }
+    }
+    return s.join("");
+};
