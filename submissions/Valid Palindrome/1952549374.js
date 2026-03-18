c27f875a78de27f716fd2504d860519e9c889b3f
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-03-18 18:29:47 -0500

    Add submission 1952549374

---
+++ submissions/Valid Palindrome/1952549374.js
@@ -0,0 +1,30 @@
+# Title: Valid Palindrome
+# Submission ID: 1952549374
+# Status: Accepted
+# Date: March 18, 2026 at 06:29:41 PM CDT
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase();
+    let i = 0; 
+    let j = s.length-1;
+    while(i<j){
+        if(!s[i].match(/[a-z0-9]/i)){
+           ++i;
+        }
+        else if(!s[j].match(/[a-z0-9]/i)){
+           --j;
+        }
+        else if(s[i] === s[j]){
+            i++;
+            j--;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+};
