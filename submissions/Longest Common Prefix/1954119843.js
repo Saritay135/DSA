70eb32c00cc5aa6c1e47b5dddacaa657cac69eab
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-03-20 12:00:22 -0500

    Add submission 1954119843

---
+++ submissions/Longest Common Prefix/1954119843.js
@@ -0,0 +1,24 @@
+# Title: Longest Common Prefix
+# Submission ID: 1954119843
+# Status: Wrong Answer
+# Date: March 20, 2026 at 12:00:17 PM CDT
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+     let ch = strs[0];
+     let res = ""
+     for(let i=1; i<strs.length;i++){
+        res = "";
+        for(let j=0; j<strs[i].length; j++){
+            if((ch.length)>j && strs[i][j] === ch[j]){
+                res = res + ch[j];
+            }
+        }
+        ch = res;
+     }
+    
+  return res;
+};
