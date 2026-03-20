51577c587f464570fe5d5ff5cb52a2f6c2508b23
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-03-20 16:36:24 -0500

    Add submission 1954304841

---
+++ submissions/Valid Anagram/1954304841.js
@@ -0,0 +1,27 @@
+# Title: Valid Anagram
+# Submission ID: 1954304841
+# Status: Accepted
+# Date: March 20, 2026 at 04:36:17 PM CDT
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let map = {};
+    if(!(s.length === t.length)) return false;
+    for(let i = 0; i < s.length; i++){
+        if(!map[s[i]]){
+            map[s[i]]=1;
+        }
+        else ++map[s[i]];
+    }
+    for(let i = 0 ; i < t.length; i++){
+        if(!map[t[i]] || map[t[i]]<0){
+            return false;
+        }
+        else --map[t[i]];
+    }
+    return true;
+};
