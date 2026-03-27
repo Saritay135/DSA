a3ea5723ad974e647434a4d861b4f6a9d7ec90e6
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-03-26 23:07:10 -0500

    Add submission 1960511738

---
+++ submissions/Valid Parentheses/1960511738.js
@@ -0,0 +1,23 @@
+# Title: Valid Parentheses
+# Submission ID: 1960511738
+# Status: Accepted
+# Date: March 26, 2026 at 11:07:04 PM CDT
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    for(let i=0; i<s.length; i++){
+        if(s[i]==="[" || s[i]==="{" || s[i]==="("){
+            stack.push(s[i]);
+        }else{
+            let top = stack.pop();
+            if(!top || (top==="[" && s[i]!=="]") || (top==="{" && s[i]!=="}") ||  (top==="(" && s[i]!==")")){
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
