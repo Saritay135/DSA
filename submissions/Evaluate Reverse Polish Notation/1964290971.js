7e710d782982eb80e4cee48af063ce2ac5499a80
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-03-30 16:56:11 -0500

    Add submission 1964290971

---
+++ submissions/Evaluate Reverse Polish Notation/1964290971.js
@@ -0,0 +1,23 @@
+# Title: Evaluate Reverse Polish Notation
+# Submission ID: 1964290971
+# Status: Accepted
+# Date: March 30, 2026 at 04:56:05 PM CDT
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let stack = [];
+    for(let i=0; i<tokens.length; i++){
+        if(tokens[i]==="+" || tokens[i]==="-" || tokens[i]==="*" || tokens[i]==="/"){
+           let a=stack.pop();
+           let  b=stack.pop();
+           let  ans = eval(`${b} ${tokens[i]} ${a}`);
+            stack.push(Math.trunc(ans));
+        }else{
+            stack.push(tokens[i]);
+        }
+    }
+ return Number(stack.pop());
+ };
