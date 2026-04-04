a852046cdaaa71c29318790c7d5f54f98d0dda31
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-04-03 22:36:24 -0500

    Add submission 1968214450

---
+++ submissions/Find Peak Element/1968214450.js
@@ -0,0 +1,22 @@
+# Title: Find Peak Element
+# Submission ID: 1968214450
+# Status: Accepted
+# Date: April 3, 2026 at 10:36:18 PM CDT
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let l = 0;
+    let r = nums.length-1;
+    while(l<r){
+        let m = l+ Math.floor((r-l)/2);
+        if(nums[m]<nums[m+1]){
+            l = m+1;
+        }else{
+            r=m;
+        }
+    }
+    return r;
+};
