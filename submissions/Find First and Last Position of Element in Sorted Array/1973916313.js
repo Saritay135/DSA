12b7869da41cd01dd53ae94cd90c2a3424f4c01e
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-04-09 14:41:20 -0500

    Add submission 1973916313

---
+++ submissions/Find First and Last Position of Element in Sorted Array/1973916313.js
@@ -0,0 +1,36 @@
+# Title: Find First and Last Position of Element in Sorted Array
+# Submission ID: 1973916313
+# Status: Accepted
+# Date: April 9, 2026 at 02:41:15 PM CDT
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let l = 0;
+     let r = nums.length-1;
+     let ans = [-1,-1];
+     while(l<=r){
+        let m = l + Math.floor((r-l)/2);
+        if(nums[m]<target){
+            l = m+1;
+        }else{
+            r=m-1;
+        }
+       if(nums[l]=== target) ans[0]=l;
+     }
+     l = 0; 
+     r = nums.length-1;
+     while(l<=r){
+        let m = l + Math.ceil((r-l)/2);
+        if(nums[m]>target){
+            r=m-1;
+        }else{
+            l = m+1;
+        }
+       if(nums[r]=== target) ans[1]=r;
+     }
+     return ans;
+};
