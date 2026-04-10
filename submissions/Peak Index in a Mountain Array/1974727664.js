ee0c641817bd95d1e9e3c2f099452924a52967bc
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-04-10 13:21:37 -0500

    Add submission 1974727664

---
+++ submissions/Peak Index in a Mountain Array/1974727664.js
@@ -0,0 +1,26 @@
+# Title: Peak Index in a Mountain Array
+# Submission ID: 1974727664
+# Status: Accepted
+# Date: April 10, 2026 at 01:21:31 PM CDT
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(arr) {
+    let l=0;
+    let r= arr.length-1;
+    while(l<=r){
+        let m = l + Math.floor((r-l)/2);
+        if(arr[m-1]<arr[m] && arr[m]>arr[m+1]){
+            l=m;
+            break;
+        }
+        else if(arr[m]<arr[m-1]){
+            r= m-1;
+        }else if(arr[m]<arr[m+1]){
+            l= m+1
+        }
+    }
+    return l;
+};
