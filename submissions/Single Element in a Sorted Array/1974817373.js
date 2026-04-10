f92526af83d580766de1a0ba1676705f39ba7de2
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-04-10 15:49:54 -0500

    Add submission 1974817373

---
+++ submissions/Single Element in a Sorted Array/1974817373.js
@@ -0,0 +1,34 @@
+# Title: Single Element in a Sorted Array
+# Submission ID: 1974817373
+# Status: Accepted
+# Date: April 10, 2026 at 03:49:48 PM CDT
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+    let l = 0; 
+    let r = nums.length-1;
+    while(l<=r){
+        let m = l+ Math.floor((r-l)/2);
+        if(nums[m-1]!= nums[m] && nums[m+1] != nums[m]){
+            r=m;
+            break;
+        }
+        if(m%2===0){
+          if(nums[m-1]=== nums[m]){
+            r=m-1;    
+        }else if(nums[m+1] === nums[m]){
+            l=m+1;
+        }
+        }else if(m%2 != 0){
+           if(nums[m-1]=== nums[m]){
+            l=m+1;    
+        }else if(nums[m+1] === nums[m]){
+            r=m-1;
+        } 
+        }
+    }
+    return nums[r];
+};
