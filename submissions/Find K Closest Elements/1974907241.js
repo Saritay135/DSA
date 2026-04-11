a06b509216f78275697c001d8a4d8a3e42d640a9
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-04-10 21:07:39 -0500

    Add submission 1974907241

---
+++ submissions/Find K Closest Elements/1974907241.js
@@ -0,0 +1,24 @@
+# Title: Find K Closest Elements
+# Submission ID: 1974907241
+# Status: Accepted
+# Date: April 10, 2026 at 09:07:33 PM CDT
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function(arr, k, x) {
+    let l = 0;
+    let r = arr.length-1;
+    while(l<r){
+        let m = l + Math.floor((r-l)/2);
+       if((arr[m+k]-x) < (x- arr[m])){
+           l = m+1;
+       } else{
+        r = m;
+       }
+    }
+    return arr.slice(l,l+k);
+};
