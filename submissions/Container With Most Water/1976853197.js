565dbb548be726c4755b31240db3efdc006e2401
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-04-12 20:50:18 -0500

    Add submission 1976853197

---
+++ submissions/Container With Most Water/1976853197.js
@@ -0,0 +1,26 @@
+# Title: Container With Most Water
+# Submission ID: 1976853197
+# Status: Accepted
+# Date: April 12, 2026 at 08:50:12 PM CDT
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let i =0;
+    let j = height.length-1;
+    let maxValue = 0;
+    while(i<=j){
+        let max = 0;
+      if(height[i]>height[j]){
+       max = height[j]*(j-i);
+       j--;
+      }else{
+        max = height[i]* (j-i);
+        i++;
+      }
+      maxValue = Math.max(maxValue,max);
+    }
+    return maxValue;
+};
