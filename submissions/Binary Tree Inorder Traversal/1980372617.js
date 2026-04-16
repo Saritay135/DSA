b271b1359fdb4b1c0e4d94497eff7642e35c676c
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-04-16 13:59:30 -0500

    Add submission 1980372617

---
+++ submissions/Binary Tree Inorder Traversal/1980372617.js
@@ -0,0 +1,32 @@
+# Title: Binary Tree Inorder Traversal
+# Submission ID: 1980372617
+# Status: Accepted
+# Date: April 16, 2026 at 01:59:24 PM CDT
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let ans = [];
+    let curr = root;
+    let stack = [];
+    while(curr || stack.length){
+        while(curr){
+            stack.push(curr);
+            curr = curr.left;
+        }
+        curr = stack.pop();
+        ans.push(curr.val);
+        curr = curr.right;
+    }
+    return ans;
+};
