6d0711c4b2fce2858569c059a1d9e4bb534ea818
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-04-20 20:56:21 -0500

    Add submission 1984051106

---
+++ submissions/Invert Binary Tree/1984051106.js
@@ -0,0 +1,29 @@
+# Title: Invert Binary Tree
+# Submission ID: 1984051106
+# Status: Accepted
+# Date: April 20, 2026 at 08:56:15 PM CDT
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if(!root) return root;
+
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+};
