bb9966cafeb30455bbd28325e813d1c3089c0b78
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-04-30 17:46:28 -0500

    Add submission 1992150544

---
+++ submissions/Binary Tree Preorder Traversal/1992150544.js
@@ -0,0 +1,29 @@
+# Title: Binary Tree Preorder Traversal
+# Submission ID: 1992150544
+# Status: Accepted
+# Date: April 30, 2026 at 05:46:22 PM CDT
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let ans = [];
+    function traversal(curr){
+        if(!curr) return;
+        ans.push(curr.val);
+        traversal(curr.left);
+        traversal(curr.right)
+    }
+   traversal(root);
+   console.log(ans)
+   return ans;
+};
