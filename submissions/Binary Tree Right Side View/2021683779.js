32e9c94eca0624b0a4c461a39ca35e83cd780fd4
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-06-03 16:39:40 -0500

    Add submission 2021683779

---
+++ submissions/Binary Tree Right Side View/2021683779.js
@@ -0,0 +1,36 @@
+# Title: Binary Tree Right Side View
+# Submission ID: 2021683779
+# Status: Accepted
+# Date: June 3, 2026 at 04:39:34 PM CDT
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if(!root) return [];
+    let ans = [];
+    let q = [root];
+
+    while(q.length){
+        let levelSize = q.length;
+        for(let i=0; i<levelSize; i++){
+            let curr = q.shift();
+
+            i==0 && ans.push(curr.val);
+
+            curr.right && q.push(curr.right);
+            curr.left && q.push(curr.left);
+        }
+    }
+
+    return ans;
+};
