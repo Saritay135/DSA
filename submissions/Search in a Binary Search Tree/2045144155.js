c1835b10ca4d02bc62ae1b2daa25da0980ab135c
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-06-24 19:34:37 -0500

    Add submission 2045144155

---
+++ submissions/Search in a Binary Search Tree/2045144155.js
@@ -0,0 +1,33 @@
+# Title: Search in a Binary Search Tree
+# Submission ID: 2045144155
+# Status: Accepted
+# Date: June 24, 2026 at 07:34:32 PM CDT
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    let ans = null;
+    let traversal = (curr) => {
+        if(curr.val == val){
+            ans = curr;
+        }
+        else if(curr.val < val){
+            curr.right && traversal(curr.right);
+        }else {
+            curr.left && traversal(curr.left);
+        }
+    }
+    traversal(root);
+    return ans;
+};
