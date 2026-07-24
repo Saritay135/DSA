b79b72e70ab2fe4ed7352d267dfd139b9040d01a
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-07-23 19:18:55 -0500

    Add submission 2078936275

---
+++ submissions/Binary Search/2078936275.js
@@ -0,0 +1,25 @@
+# Title: Binary Search
+# Submission ID: 2078936275
+# Status: Wrong Answer
+# Date: July 23, 2026 at 07:18:50 PM CDT
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let right = nums.length - 1;
+    let left = 0;
+    while(right>left){
+        let middle = Math.floor((left+right)/2);
+        if(nums[middle] === target){
+            return middle;
+        } else if(nums[middle]>target){
+            right = middle-1;
+        } else {
+            left = middle +1
+        }
+    }
+    return -1;
+};
