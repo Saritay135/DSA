e5e6b05ea54aab68de8762db5a6ac56350c6275a
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-07-24 11:50:12 -0500

    Add submission 2079841097

---
+++ submissions/Sort an Array/2079841097.js
@@ -0,0 +1,23 @@
+# Title: Sort an Array
+# Submission ID: 2079841097
+# Status: Time Limit Exceeded
+# Date: July 24, 2026 at 11:50:06 AM CDT
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+  let n = nums.length;
+  for(let i=1; i<n ;i++){
+    let curr= nums[i];
+    let prev = i-1;
+    while(nums[prev]>curr && prev >=0){
+        nums[prev+1]= nums[prev];
+        nums[prev]= curr;
+        prev--;
+    }
+  }
+   return nums;
+};
+
