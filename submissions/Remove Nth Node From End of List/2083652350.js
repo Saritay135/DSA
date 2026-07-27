4c8021d004e25d1679f56af4dda6278e12ca483f
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-07-27 11:35:17 -0500

    Add submission 2083652350

---
+++ submissions/Remove Nth Node From End of List/2083652350.js
@@ -0,0 +1,32 @@
+# Title: Remove Nth Node From End of List
+# Submission ID: 2083652350
+# Status: Accepted
+# Date: July 27, 2026 at 11:35:11 AM CDT
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let sentinel = new ListNode();
+    sentinel.next = head;
+    let slow = sentinel;
+    let fast = head;
+    for(let i=0; i<n;i++){
+        fast = fast.next;
+    }
+    while(fast){
+        slow = slow.next;
+        fast = fast.next;
+    }
+    slow.next = slow.next.next;
+    return sentinel.next;
+};
