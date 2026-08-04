54dc70a00e8bb260dcc3dd39d09fa55e02c61a44
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-08-03 20:24:19 -0500

    Add submission 2093297473

---
+++ submissions/Top K Frequent Elements/2093297473.js
@@ -0,0 +1,30 @@
+# Title: Top K Frequent Elements
+# Submission ID: 2093297473
+# Status: Accepted
+# Date: August 3, 2026 at 08:24:13 PM CDT
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map = {};
+
+    for(let i=0;i<nums.length; i++){
+        if(!map[nums[i]]) map[nums[i]]=0;
+        ++map[nums[i]];
+    }
+
+    let pq = new MinPriorityQueue(x=>x.freq);
+
+    for(let key in map){
+        pq.push({val:key, freq:map[key]});
+   
+    if(pq.size()>k){
+        pq.pop();
+    }
+    }
+
+    return pq.toArray().map(x=> Number(x.val));
+};
