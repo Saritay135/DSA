714b372db081bc5ff18aac1a2308acb58591adba
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-08-04 20:28:40 -0500

    Add submission 2094678693

---
+++ submissions/Kth Smallest Element in a Sorted Matrix/2094678693.js
@@ -0,0 +1,27 @@
+# Title: Kth Smallest Element in a Sorted Matrix
+# Submission ID: 2094678693
+# Status: Accepted
+# Date: August 4, 2026 at 08:28:35 PM CDT
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(matrix, k) {
+    let n = matrix[0].length;
+    let heap = new MinPriorityQueue(x=>x.val);
+    for(let i=0; i<n; i++){
+        heap.push({val:matrix[i][0], row:i, col:0})
+    }
+
+    for(let count=0; count<k-1;count++){
+        let {val,row,col} = heap.pop();
+
+        if(col+1<n){
+            heap.push({val:matrix[row][col+1], row:row, col:col+1});
+        }
+
+    }
+    return heap.pop().val;
+};
