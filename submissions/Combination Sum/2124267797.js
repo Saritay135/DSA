ad9e7cb354ffd976dec2adad0307f87c96240f25
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-08-29 13:52:10 -0500

    Add submission 2124267797

---
+++ submissions/Combination Sum/2124267797.js
@@ -0,0 +1,24 @@
+# Title: Combination Sum
+# Submission ID: 2124267797
+# Status: Accepted
+# Date: August 29, 2026 at 01:52:03 PM CDT
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(arr, target) {
+    let result = [];
+    let backtrack = (remainingSum, path, start)=>{
+        if(remainingSum===0) result.push([...path]);
+        if(remainingSum<=0) return;
+        for(let i=start; i<arr.length;i++){
+            path.push(arr[i]);
+            backtrack(remainingSum-arr[i],path,i);
+            path.pop();
+        }
+    }
+    backtrack(target,[],0);
+    return result;
+};
