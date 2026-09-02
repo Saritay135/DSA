ef8a8c1d391127929653f60861dcf04d83b86fdc
Saritay135 <134283073+Saritay135@users.noreply.github.com>
2026-09-02 13:04:37 -0500

    Add submission 2128817219

---
+++ submissions/Permutations II/2128817219.js
@@ -0,0 +1,29 @@
+# Title: Permutations II
+# Submission ID: 2128817219
+# Status: Accepted
+# Date: September 2, 2026 at 01:04:30 PM CDT
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    let result =[];
+    nums.sort((a,b) =>(a-b));
+    
+     let backtrack = function(path, arr) {
+        if(path.length === nums.length){
+        result.push([...path]);
+        return;
+    }
+       for(let i=0; i<arr.length; i++){
+        if(i>0 && arr[i]===arr[i-1]) continue;
+
+        path.push(arr[i]);
+        backtrack(path, [...arr.slice(0,i), ...arr.slice(i+1)]);
+        path.pop();
+       }
+     }
+     backtrack([],nums);
+     return result;
+};
